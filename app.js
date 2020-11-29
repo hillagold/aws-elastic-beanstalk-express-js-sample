4b5845e28b4f3ee6f985e96fd9f19392ebffae88
unknown <meracez5@gmail.com>
2020-11-29 22:12:01 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hilla is in the house!'));
+app.get('/', (req, res) => res.send('Hilla is in the house and no one can STOP her now!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
